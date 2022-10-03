13d9076a5b96330d66e14a5f31faf0c60773b842
Mossyegghead01 <luthfihaidar1176@gmail.com>
2022-10-03 15:30:18 +0700

    Fixed some typo
    -Fixed some typo

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,7 @@ export default function Projects() {
     return (
         <>
             Welp you found me!
-            I don't know how you manage to find me but this page is not ready yet. COme back when it's ready :)
+            I don't know how you manage to find me but this page is not ready yet. Come back when it's ready :)
         </>
     )
 }
